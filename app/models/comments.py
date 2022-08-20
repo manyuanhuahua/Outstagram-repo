e15f39cb8f0db9442e0aa1c090833dd8e195d098
ting <manyuanhuahua90@gmail.com>
2022-08-19 17:19:36 -0700

    comment back all models

--- app/models/comments.py
+++ app/models/comments.py
@@ -1,25 +1,25 @@
-# from .db import db
-# from sqlalchemy.orm import relationship
-# from sqlalchemy import ForeignKey
-# # from flask_login import UserMixin
+from .db import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+# from flask_login import UserMixin
 
-# class Comment(db.Model):
-#   __tablename__ = "comments"
+class Comment(db.Model):
+  __tablename__ = "comments"
 
-#   id = db.Column(db.Integer, primary_key=True)
-#   userId = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
-#   postId = db.Column(db.Integer)
-#   content = db.Column(db.String(500))
-#   postedAt = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+  id = db.Column(db.Integer, primary_key=True)
+  userId = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
+  postId = db.Column(db.Integer)
+  content = db.Column(db.String(500))
+  postedAt = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
-#   user = relationship("User", back_populates="comments")
-#   posts = relationship("Post", back_populates="comments")
+  user = relationship("User", back_populates="comments")
+  posts = relationship("Post", back_populates="comments")
 
-#   def to_dict(self):
-#     return {
-#       "id": self.id,
-#       "userId": self.userId,
-#       "postId": self.postId,
-#       "content": self.content,
-#       "postedAt": self.postedAt
-#     }
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "userId": self.userId,
+      "postId": self.postId,
+      "content": self.content,
+      "postedAt": self.postedAt
+    }
--- app/models/post.py
+++ app/models/post.py
@@ -1,28 +1,28 @@
-# from .db import db
-# from sqlalchemy.orm import relationship
-# from sqlalchemy import ForeignKey
-# # from flask_login import UserMixin
+from .db import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+# from flask_login import UserMixin
 
-# class Post(db.Model):
-#   __tablename__ = "posts"
+class Post(db.Model):
+  __tablename__ = "posts"
 
-#   id = db.Column(db.Integer, primary_key=True)
-#   userId = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
-#   description = db.Column(db.String(500))
-#   imageUrl = db.Column(db.String(500))
-#   postedAt = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+  id = db.Column(db.Integer, primary_key=True)
+  userId = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
+  description = db.Column(db.String(500))
+  imageUrl = db.Column(db.String(500))
+  postedAt = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
-#   user = relationship("User", back_populates="posts")
-#   likes = relationship("Like", back_populates="posts")
-#   comments = relationship("Comment", back_populates="posts")
+  user = relationship("User", back_populates="posts")
+  likes = relationship("Like", back_populates="posts")
+  comments = relationship("Comment", back_populates="posts")
 
 
-#   def to_dict(self):
-#     return {
-#       "id": self.id,
-#       "userId": self.userId,
-#       "description": self.description,
-#       "imageUrl": self.imageUrl,
-#       "postedAt": self.postedAt,
-#       "username": self.user.username
-#     }
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "userId": self.userId,
+      "description": self.description,
+      "imageUrl": self.imageUrl,
+      "postedAt": self.postedAt,
+      "username": self.user.username
+    }
