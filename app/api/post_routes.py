80aeb46c167541567eefcf2e5ee4cce682470fb0
TING FENG <ting@Ting.localdomain>
2022-08-22 11:14:06 -0700

    get others post done

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -6,12 +6,35 @@ from app.models import Post
 post_routes = Blueprint('posts', __name__)
 
 #get all posts for the session user
-@post_routes.route('/')
+@post_routes.route('/user/session')
 @login_required
-def posts():
+def get_all_posts():
     posts = Post.query.filter(Post.userId == current_user.id).all()
     res = {}
 
+    for post in posts:
+        like_status=list(filter(lambda user: user.id==current_user.id, post.post_like_users))
+        res[post.id]={
+            "id": post.id,
+            "user_id": post.userId,
+            "description": post.description,
+            "image_url": post.image_url,
+            "created_at": post.created_at,
+            "user": {
+                "profile_image":post.user.profile_image,
+                "username":post.user.username,
+            },
+            "total_comments": len(post.comments),
+            "total_likes":len(post.post_like_users),
+            "like_status":True if len(like_status) > 0 else False,
+        }
+    return {"Posts": res}
+
+@post_routes.route('/user/<int:id>')
+@login_required
+def get_others_posts(id):
+    posts = Post.query.filter(Post.userId == id).all()
+    res = {}
     for post in posts:
         like_status=list(filter(lambda user: user.id==current_user.id, post.post_like_users))
         res[post.id]={
