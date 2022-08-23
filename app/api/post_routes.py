7fbaea97faf1d4c3ed55a2967f5084ac89253ca3
zhihong <zhihong.liu81@gmail.com>
2022-08-22 22:17:37 -0500

    comment CRUD done

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, Response,request
 # from flask_api import status
 from flask_login import login_required, current_user
-from app.models import Post,db
+from app.models import Post,db, Comment
 from app.forms.create_post import CreatePostForm
+from app.forms.create_comment import CreateCommentForm
 
 
 post_routes = Blueprint('posts', __name__)
@@ -122,9 +123,13 @@ def create_post():
 @post_routes.route('/<int:postId>',methods=['PUT'])
 @login_required
 def update_post(postId):
+    post = Post.query.get(postId)
+    if post.userId != current_user.id:
+        return {"errors": ['Unauthorized']}, 401
+
     form = CreatePostForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    post = Post.query.get(postId)
+
     if post.image_url != form.data['image_url']:
         return {'errors': ['image cannot be changed']}, 400
 
@@ -154,6 +159,129 @@ def delete_post(postId):
     post = Post.query.get(postId)
     if not post:
         return {'errors': ['post cannot be found']}, 400
+
+    if post.userId != current_user.id:
+        return {"errors": ['Unauthorized']}, 401
+
     db.session.delete(post)
+    db.session.commit()
+    return {"message":"Successfully deleted"}
+
+
+#get all comments for a specified post
+@post_routes.route('/<int:postId>/comments')
+@login_required
+def get_all_comments(postId):
+    post = Post.query.get(postId)
+    if not post:
+        return {'errors': ['post can not be found']}, 404
+    comments = Comment.query.filter(Comment.postId == postId).all()
+    res = {}
+    for comment in comments:
+        like_status = list(filter(lambda user: user.id==current_user.id, comment.comment_like_users))
+        res[comment.id] = {
+            "id": comment.id,
+            "userId": comment.userId,
+            "postId": comment.postId,
+            "user": {
+                "profileImage": comment.user.profile_image,
+                "username": comment.user.username
+            },
+            "content": comment.content,
+            "likeStatus": True if len(like_status) > 0 else False,
+            "totalLikes": len(comment.comment_like_users),
+            "createdAt": comment.created_at
+        }
+    return {"Comments": res}
+
+#Create a comment
+@post_routes.route('/<int:postId>/comments/new', methods=["POST"])
+@login_required
+def create_comments(postId):
+    post = Post.query.get(postId)
+    if not post:
+        return {'errors': ['post can not be found']}, 404
+
+    form = CreateCommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        comment = Comment(
+            content=form.data['content'],
+        )
+        comment.userId = current_user.id
+        comment.postId = postId
+        db.session.add(comment)
+        db.session.commit()
+
+        res = {
+            "id": comment.id,
+            "userId": comment.userId,
+            "content": comment.content,
+            "createdAt": comment.created_at,
+            "user": {
+                "profileImage":comment.user.profile_image,
+                "username":comment.user.username
+            },
+            "total_likes":len(comment.comment_like_users),
+            "like_status": False
+        }
+        return res
+    return  {'errors': ['content is required']}, 400
+
+
+#Update a comment
+@post_routes.route('/<int:postId>/comments/<int:commentId>', methods=["PUT"])
+@login_required
+def update_comments(postId, commentId):
+    post = Post.query.get(postId)
+    if not post:
+        return {'errors': ['post can not be found']}, 404
+
+    comment = Comment.query.get(commentId)
+    if not comment:
+        return {'errors': ['comment can not be found']}, 404
+
+    if comment.userId != current_user.id:
+        return {"errors": ['Unauthorized']}, 401
+
+    form = CreateCommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        comment.content = form.data["content"]
+        db.session.commit()
+        like_status = list(filter(lambda user: user.id==current_user.id, comment.comment_like_users))
+        res = {
+            "id": comment.id,
+            "userId": comment.userId,
+            "content": comment.content,
+            "createdAt": comment.created_at,
+            "user": {
+                "profileImage":comment.user.profile_image,
+                "username":comment.user.username
+            },
+            "totalLikes":len(comment.comment_like_users),
+            "likeStatus": True if len(like_status) > 0 else False
+        }
+        return res
+    return  {'errors': ['content is required']}, 400
+
+
+
+#delete a comment
+@post_routes.route('/<int:postId>/comments/<int:commentId>',methods=['DELETE'])
+@login_required
+def delete_comment(postId, commentId):
+    post = Post.query.get(postId)
+    if not post:
+        return {'errors': ['post cannot be found']}, 400
+
+    comment = Comment.query.get(commentId)
+    if not comment:
+        return {'errors': ['comment cannot be found']}, 400
+
+    if comment.userId != current_user.id:
+        return {"errors": ['Unauthorized']}, 401
+
+    db.session.delete(comment)
     db.session.commit()
     return {"message":"Successfully deleted"}
