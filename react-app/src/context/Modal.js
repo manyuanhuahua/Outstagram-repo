5372063b71dedf2cd65bfb683e657f8415095e94
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-08-26 15:16:01 -0700

    fixed alignment for modal

--- react-app/src/context/Modal.js
+++ react-app/src/context/Modal.js
@@ -27,9 +27,9 @@ export function Modal({ onClose, children }) {
   if (!modalNode) return null;
 
   return ReactDOM.createPortal(
-    <div id="modal" >
+    <div id="modal">
       <div id="modal-background" onClick={onClose} />
-      <div id="modal-content" style={{ borderRadius: '0.5rem' }}>
+      <div id="modal-content" style={{ borderRadius: '0.5rem', marginLeft: '15px' }}>
         {children}
       </div>
     </div>,
